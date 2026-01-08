ff9131d9b9f0e597525f7bd805b1055d3ae37e97
CodeDhairya100 <dhairyaprofilo@gmail.com>
2026-01-08 20:53:42 +0530

    Home Directory

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div className='min-h-screen bg-black text-white overflow-x-hidden'>
+        {/* Background */}
+    </div>
+  )
+}
+
+export default Home
